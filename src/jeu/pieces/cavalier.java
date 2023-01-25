5e451a9e39b461f01443850b873ea36855e66b2e
zertyfaucher <83868183+zertyfaucher@users.noreply.github.com>
2023-01-25 11:14:32 +0100

    cavalier finit

--- src/jeu/pieces/cavalier.java
+++ src/jeu/pieces/cavalier.java
@@ -12,6 +12,11 @@ public class cavalier extends pieces {
     }
 
     public boolean coupPossible(couleur c, int x, int y, int x2, int y2){
+        if (x2<0 || x2>7 || y2<0 || y2 > 7)// a voir
+            return false;
+        if (((x == x2+1||x==x2-1)&&(y==y2-2||y==y2+2))||((x==x2-2||x==x2+2)&&(y==y2-1||y==y2+1)))
+            if (p.getCase(x2, y2)==null||p.getCase(x2, y2).getCouleur()!=c)
+                return true;
         return false;
     }
 }
