1306a204b0083fb353f25dd857fd0325d8a855fb
zertyfaucher <83868183+zertyfaucher@users.noreply.github.com>
2023-01-31 12:06:43 +0100

    tour finish

--- src/jeu/pieces/tour.java
+++ src/jeu/pieces/tour.java
@@ -8,6 +8,41 @@ public class tour extends pieces {
     }
 
     public boolean coupPossible(couleur c, int x, int y, int x2, int y2){
+        if (x2<0 || x2>7 || y2<0 || y2 > 7)// a voir
+            return false;
+
+        if (x != x2 && y==y2){
+            if (x > x2) {
+                for (int i = 1; i < x - x2; i++)
+                    if (p.getCase(x - i, y) != null)
+                        return false;
+                if (p.getCase(x2,y2)==null|| p.getCase(x2,y2).getCouleur()!=c)
+                    return true;
+                return false;
+            }
+            for (int i = 1; i < x - x2; i++)
+                if (p.getCase(x + i, y) != null)
+                    return false;
+            if (p.getCase(x2,y2)==null|| p.getCase(x2,y2).getCouleur()!=c)
+                return true;
+            return false;
+        }
+        if (x == x2 && y!=y2){
+            if (y > y2){
+                for (int i = 1; i < x - x2; i++)
+                    if (p.getCase(x, y - i) != null)
+                        return false;
+                if (p.getCase(x2,y2)==null|| p.getCase(x2,y2).getCouleur()!=c)
+                    return true;
+                return false;
+            }
+            for (int i = 1; i < x - x2; i++)
+                if (p.getCase(x, y - i) != null)
+                    return false;
+            if (p.getCase(x2,y2)==null|| p.getCase(x2,y2).getCouleur()!=c)
+                return true;
+            return false;
+        }
         return false;
     }
 
